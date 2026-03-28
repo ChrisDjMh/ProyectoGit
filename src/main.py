330d93fdbed7baa156f66f8269b5c2b492c6f850
ChrisDjMh <chrisdjmh@gmail.com>
2026-03-27 17:36:10 -0700

    Resolviendo conflictos e integrando funcionalidades

--- src/main.py
+++ src/main.py
@@ -1,9 +1,6 @@
 print('Versión Inicial')
-<<<<<<< HEAD
 print('Login')
 print('BaseDeDatos')
 print('FuncionesPrincipales')
 print('Funciones BackEnd')
-=======
 print('Funciones FrontEnd')
->>>>>>> FrontEnd2
